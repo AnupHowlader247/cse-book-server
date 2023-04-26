8f7eceb694fe46c5a9cae8245e03ded694fe8502
AnupHowlader247 <anuphowlader247@gmail.com>
2023-04-26 22:51:02 +0600

    search

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middle wares
+// middle wares 
 app.use(cors());
 app.use(express.json());
 
@@ -20,12 +20,12 @@ const client = new MongoClient(uri, {
 async function run() {
   const userCollection = client.db("CSE-BOOK").collection("users");
   const postCollection = client.db("CSE-BOOK").collection("posts");
+  const messagesCollection = client.db("CSE-BOOK").collection("messages");
   
   try {
     //insert userinfo into the database
     app.post("/storeUser", async (req, res) => {
       const userInfo = req.body;
-      console.log(userInfo)
       const result = await userCollection.insertOne(userInfo);
       res.send(result);
     });
@@ -35,17 +35,40 @@ async function run() {
       const result = await postCollection.insertOne(post);
       res.send(result);
     });
-     //login user
-     app.post("/loginUser", async (req, res) => {
-      const userInfo = req.body;
-      const result = await userCollection.findOne({email: userInfo.email});
-      if(result.password === userInfo.password){
-        res.send({success: true , data: result})
-      }
-      else{
-        res.send({success: false , data: result})
-      }
+     //insert messages
+     app.post("/storeMessages", async (req, res) => {
+      const post = req.body;
+      const result = await messagesCollection.insertOne(post);
+      res.send(result);
     });
+
+     //get all post
+     app.get("/allPost", async (req, res) => {
+      const result = await postCollection.find({}).toArray();
+      res.send(result);
+    });
+
+     //get search post
+     app.get("/searchPost/:name", async (req, res) => {
+      const name = req.params.name
+      const result = await postCollection.find({topic:name}).toArray();
+      res.send(result);
+    });
+
+     //get all chat users
+     app.get("/allchatUsers/:name", async (req, res) => {
+      const name = req.params.name;
+      const result = await messagesCollection.find({ $or: [ { to:name }, { sender:name } ] }).toArray();
+      res.send(result);
+    });
+
+      //get all chat with individual
+      app.get("/individualChat/:name", async (req, res) => {
+        const name = req.params.name;
+        const result = await messagesCollection.find({ $or: [ { to:name.split('&')[0], sender: name.split('&')[1] }, { to:name.split('&')[1], sender: name.split('&')[0]  } ] }).toArray();
+        res.send(result);
+      });
+
   }
   catch (error) {
     console.log(error) 
